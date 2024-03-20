33cfef9f7e70c6a24ce4510331ef18a9d99be499
LSantAnnaB <l.sant.anna.b@gmail.com>
2024-03-20 10:13:03 -0300

    start

---
+++ src/test/java/com/santanna/picpaydesafio/PicpayDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santanna.picpaydesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
