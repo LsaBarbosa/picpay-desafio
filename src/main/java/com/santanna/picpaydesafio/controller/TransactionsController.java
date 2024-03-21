72bd2a86f19f0ce750939825a2796a99f730dd73
LSantAnnaB <l.sant.anna.b@gmail.com>
2024-03-21 09:26:46 -0300

    feat Controller + DTO

---
+++ src/main/java/com/santanna/picpaydesafio/controller/TransactionsController.java
@@ -0,0 +1,25 @@
+package com.santanna.picpaydesafio.controller;
+
+import com.santanna.picpaydesafio.domain.dto.TransactionDTO;
+import com.santanna.picpaydesafio.domain.transactional.Transaction;
+import com.santanna.picpaydesafio.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("transactions")
+public class TransactionsController {
+    @Autowired
+    private  TransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<Transaction> createTransaction (@RequestBody TransactionDTO transactionDTO) throws Exception {
+        Transaction newTransaction = this.transactionService.createTransactional(transactionDTO);
+        return new ResponseEntity<>(newTransaction, HttpStatus.CREATED);
+    }
+}
---
+++ src/main/java/com/santanna/picpaydesafio/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.santanna.picpaydesafio.controller;
+
+import com.santanna.picpaydesafio.domain.dto.UserDTO;
+import com.santanna.picpaydesafio.domain.user.User;
+import com.santanna.picpaydesafio.service.UserServcie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserServcie userServcie;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody UserDTO userDTO) {
+        User newUser = userServcie.createUser(userDTO);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = this.userServcie.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+
+}
